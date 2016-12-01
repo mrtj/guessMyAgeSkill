0b06798105e33ab04243c461f16f9095cc6ec189
Janos Tolgyesi <janos.tolgyesi@neosperience.com>
2016-12-01 08:37:13 -0800

    Document code

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@
 
 /**
  * Alexa skill "Guess my age"
+ *
+ * Guess my age is a small gag Alexa skill that asks you to perform some calculations with the digits of your age.
+ * When you tell the final figure of your calculations, it will tell your age in years.
  **/
 
 'use strict';
@@ -11,6 +14,9 @@ const Alexa = require('alexa-sdk');
 
 const APP_ID = 'amzn1.ask.skill.2cd6bfc9-1307-4c51-b3c7-f909b29f207e';
 
+/**
+ * Localized texts
+ */
 const languageStrings = {
     'en-GB': {
         translation: {
@@ -56,30 +62,48 @@ const languageStrings = {
     }
 };
 
+/**
+ * States of the skill
+ */
 var states = {
+
+    /** The skill is being started */
     START: '_START',
+
+    /** The skill is asking the user to perform the calculations */
     STEPS: '_STEPS',
+
+    /** The skill is waiting for the calculation result from the user */
     RESULT: '_RESULT'
 };
 
-
+/**
+ * Generic intent handlers valid in all states
+ */
 const genericHandlers = {
+
+    // Start a new game
     'NewSession': function () {
         this.emit('GuessMyAge');
     },
+
+    // Initial intent
     'GuessMyAge': function () {
         this.handler.state = states.START;
         this.emit(':ask', this.t('WELCOME_MESSAGE'), this.t('WELCOME_REPROMPT'));
     },
-    'AMAZON.HelpIntent': function () {
-        this.emit(':ask', this.t('HELP_MESSAGE'), this.t('HELP_MESSAGE'));
-    },
+
+    // Cancel the game
     'AMAZON.CancelIntent': function () {
         this.emit(':tell', this.t('STOP_MESSAGE'));
     },
+
+    // Stop the game
     'AMAZON.StopIntent': function () {
         this.emit(':tell', this.t('STOP_MESSAGE'));
     },
+
+    // Session ended, clear session variables (in future when they might be persisted)
     'SessionEndedRequest': function () {
         this.attributes['currentStep'] = undefined;
         this.handler.state = undefined;
@@ -87,17 +111,12 @@ const genericHandlers = {
     }
 };
 
+/**
+ * Specific handlers valid in START state
+ */
 const startHandlers = {
-    'OkIntent': function () {
-        this.handler.state = states.STEPS;
-        var currentStep = 0;
-        this.attributes['currentStep'] = currentStep;
-        var stepMessage = this.t('STEPS')[currentStep];
-        this.emit(':ask', stepMessage, this.t('REPROMPT_OK'));
-    },
-    'ResultIntent': function () {
-        this.emit(':ask', this.t('EARLY_RESULT'), this.t('REPROMPT_OK'));
-    },
+
+    // Starts the game: initializes session variables and asks the first calculation step
     'AMAZON.YesIntent': function () {
         this.handler.state = states.STEPS;
         var currentStep = 0;
@@ -105,37 +124,79 @@ const startHandlers = {
         var stepMessage = this.t('STEPS')[currentStep];
         this.emit(':ask', stepMessage, this.t('REPROMPT_OK'));
     },
+
+    // TODO: why does this.emit('AMAZON.YesIntent') does not work?
+    'OkIntent': function () {
+        this.handler.state = states.STEPS;
+        var currentStep = 0;
+        this.attributes['currentStep'] = currentStep;
+        var stepMessage = this.t('STEPS')[currentStep];
+        this.emit(':ask', stepMessage, this.t('REPROMPT_OK'));
+    },
+
+    // Too early to say the result yet
+    'ResultIntent': function () {
+        this.emit(':ask', this.t('EARLY_RESULT'), this.t('REPROMPT_OK'));
+    },
+
+    // User does not want to play
     'AMAZON.NoIntent': function(){
         this.emit(':tell', this.t('STOP_MESSAGE'));
+    },
+
+    // Help intent
+    'AMAZON.HelpIntent': function () {
+        this.emit(':ask', this.t('HELP_MESSAGE'), this.t('HELP_MESSAGE'));
     }
 };
 
+/**
+ * The union of generic state handlers and start handlers, to be associated with the START state
+ */
 const startHandlerGroup = Alexa.CreateStateHandler(states.START, Object.assign({}, genericHandlers, startHandlers));
 
+/**
+ * Specific handlers valid in STEP state
+ */
 const stepHandlers = {
+
+    // This intent iterates through all steps. It saves the current step index to the session variables
     'OkIntent': function () {
         var currentStep = this.attributes['currentStep'] + 1;
         var numberOfSteps = this.t('STEPS').length;
         if (currentStep < numberOfSteps) {
             this.attributes['currentStep'] = currentStep;
             var stepMessage = this.t('STEPS')[currentStep];
+            // If this is the last step, switch to RESULT state
             if (currentStep == numberOfSteps - 1) {
                 this.handler.state = states.RESULT;
             }
             this.emit(':ask', stepMessage, this.t('REPROMPT_OK'));
         }
     },
+
+    // Too early to say the result yet
     'ResultIntent': function () {
         this.emit(':ask', this.t('EARLY_RESULT'), this.t('REPROMPT_OK'));
     }
 };
 
+/**
+ * The union of generic state handlers and step handlers, to be associated with the START state
+ */
 const stepHandlerGroup = Alexa.CreateStateHandler(states.STEPS, Object.assign({}, genericHandlers, stepHandlers));
 
+/**
+ * Specific handlers valid in RESULT state
+ */
 const resultHandlers = {
+
+    // We are expecting the result now
     'OkIntent': function () {
         this.emit(':ask', this.t('REPROMPT_RESULT'), this.t('REPROMPT_RESULT'));
     },
+
+    // Calculate the age based on the result of the user
     'ResultIntent': function () {
         this.handler.state = undefined;
         this.attributes['currentStep'] = undefined;
@@ -145,8 +206,14 @@ const resultHandlers = {
     }
 };
 
+/**
+ * The union of generic state handlers and result handlers, to be associated with the START state
+ */
 const resultHandlerGroup = Alexa.CreateStateHandler(states.RESULT, Object.assign({}, genericHandlers, resultHandlers));
 
+/**
+ * Lambda handler, simple launches Alexa SDK
+ */
 exports.handler = (event, context) => {
     console.log('Event: ' + JSON.stringify(event));
     const alexa = Alexa.handler(event, context);
